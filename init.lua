a03798beb48ce11a2c42d83851ff42fa92e74fa6
Michael Lawrence <mjl93@me.com>
2024-12-27 18:15:22 -0800

    Theme change

--- init.lua
+++ init.lua
@@ -782,69 +782,69 @@ require('lazy').setup({
   --
   {
     --'folke/tokyonight.nvim',
-    'navarasu/onedark.nvim',
+    -- 'navarasu/onedark.nvim',
     --'ellisonleao/gruvbox.nvim',
     --'kdheepak/monochrome.nvim',
-    --'catppuccin/nvim',
+    'catppuccin/nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
-      -- require('catppuccin').setup {
-      --   flavour = 'frappe', -- latte, frappe, macchiato, mocha
-      --   background = { -- :h background
-      --     light = 'latte',
-      --     dark = 'frappe',
-      --   },
-      --   transparent_background = false, -- disables setting the background color.
-      --   show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-      --   term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
-      --   dim_inactive = {
-      --     enabled = false, -- dims the background color of inactive window
-      --     shade = 'dark',
-      --     percentage = 0.15, -- percentage of the shade to apply to the inactive window
-      --   },
-      --   no_italic = false, -- Force no italic
-      --   no_bold = false, -- Force no bold
-      --   no_underline = false, -- Force no underline
-      --   styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-      --     comments = { 'italic' }, -- Change the style of comments
-      --     conditionals = { 'italic' },
-      --     loops = {},
-      --     functions = {},
-      --     keywords = {},
-      --     strings = {},
-      --     variables = {},
-      --     numbers = {},
-      --     booleans = {},
-      --     properties = {},
-      --     types = {},
-      --     operators = {},
-      --     -- miscs = {}, -- Uncomment to turn off hard-coded styles
-      --   },
-      --   color_overrides = {},
-      --   custom_highlights = {},
-      --   default_integrations = true,
-      --   integrations = {
-      --     cmp = true,
-      --     gitsigns = true,
-      --     nvimtree = true,
-      --     treesitter = true,
-      --     notify = false,
-      --     mini = {
-      --       enabled = true,
-      --       indentscope_color = '',
-      --     },
-      --     -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-      --   },
-      -- }
-      -- vim.cmd 'colorscheme catppuccin'
-      -- Lua
-      require('onedark').setup {
-        style = 'darker',
-        transparent = true,
+      require('catppuccin').setup {
+        flavour = 'macchiato', -- latte, frappe, macchiato, mocha
+        background = { -- :h background
+          light = 'latte',
+          dark = 'macchiato',
+        },
+        transparent_background = false, -- disables setting the background color.
+        show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+        term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
+        dim_inactive = {
+          enabled = false, -- dims the background color of inactive window
+          shade = 'dark',
+          percentage = 0.15, -- percentage of the shade to apply to the inactive window
+        },
+        no_italic = false, -- Force no italic
+        no_bold = false, -- Force no bold
+        no_underline = false, -- Force no underline
+        styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+          comments = { 'italic' }, -- Change the style of comments
+          conditionals = { 'italic' },
+          loops = {},
+          functions = {},
+          keywords = {},
+          strings = {},
+          variables = {},
+          numbers = {},
+          booleans = {},
+          properties = {},
+          types = {},
+          operators = {},
+          -- miscs = {}, -- Uncomment to turn off hard-coded styles
+        },
+        color_overrides = {},
+        custom_highlights = {},
+        default_integrations = true,
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          treesitter = true,
+          notify = false,
+          mini = {
+            enabled = true,
+            indentscope_color = '',
+          },
+          -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+        },
       }
-      require('onedark').load()
-      vim.cmd 'colorscheme onedark'
-      vim.cmd.hi 'Comment gui=none'
+      vim.cmd 'colorscheme catppuccin'
+      -- Lua
+      --      require('onedark').setup {
+      --        style = 'darker',
+      --        transparent = true,
+      --      }
+      --      require('onedark').load()
+      --      vim.cmd 'colorscheme onedark'
+      --      vim.cmd.hi 'Comment gui=none'
     end,
   },
 
